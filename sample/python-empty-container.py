85ac3ce7f420a37e035b9849eac4b49331646254
Xiaoxin Zhao <xxz@amazon.com>
2021-06-04 14:03:01 -0700

    Remove one test case in empty container.

--- sample/python-empty-container.py
+++ sample/python-empty-container.py
@@ -5,15 +5,6 @@ def non_conformant_1():
     else:
         return False
 
-
-def non_conformant_2():
-    val = []
-    if len(val) > 0:
-        return True
-    else:
-        return False
-
-
 def non_conformant_3():
     val = [1, 2, 3]
     if len(val) > 0:
